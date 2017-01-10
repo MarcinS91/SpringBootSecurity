508c86c7a2f6bb7c93a745e0c837c65a9d4a997c
MarcinS91 <szewc.marcin91@gmail.com>
2017-01-10 23:02:32 +0100

    commit

--- SpringBootSecurity/src/main/java/com/security/model/Users.java
+++ SpringBootSecurity/src/main/java/com/security/model/Users.java
@@ -2,4 +2,41 @@ package com.security.model;
 
 public class Users {
 
+	private int userId;
+	private String username;
+	private String password;
+	private Boolean enable;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getEnable() {
+		return enable;
+	}
+
+	public void setEnable(Boolean enable) {
+		this.enable = enable;
+	}
+
 }
