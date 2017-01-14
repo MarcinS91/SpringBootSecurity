afe785a9d49e188d40b6f9dba3c16c8204102042
MarcinS91 <szewc.marcin91@gmail.com>
2017-01-14 17:37:52 +0100

    commit

--- SpringBootSecurity/src/main/java/com/security/admin/AdminHome.java
+++ SpringBootSecurity/src/main/java/com/security/admin/AdminHome.java
@@ -1,9 +1,13 @@
 package com.security.admin;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.security.model.Customer;
 import com.security.service.CustomerService;
 
 @Controller
@@ -12,9 +16,17 @@ public class AdminHome {
 
 	@Autowired
 	CustomerService customerService;
-	
+
 	@RequestMapping
 	public String adminPage() {
 		return "admin";
+	}
+
+	@RequestMapping("/customer")
+	public String customerManagement(Model model) {
+		List<Customer> customerList = customerService.getAllCustomers();
+		model.addAttribute("customerList", customerList);
+
+		return "customerManagement";
 	}
 }
