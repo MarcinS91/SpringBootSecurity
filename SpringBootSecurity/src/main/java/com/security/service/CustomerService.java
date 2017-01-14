23462a4b82b502759713248bf8c260e3131d3ca6
MarcinS91 <szewc.marcin91@gmail.com>
2017-01-14 15:45:34 +0100

    commit

--- SpringBootSecurity/src/main/java/com/security/service/CustomerService.java
+++ SpringBootSecurity/src/main/java/com/security/service/CustomerService.java
@@ -1,11 +1,10 @@
-package com.security.dao;
+package com.security.service;
 
 import java.util.List;
 
 import com.security.model.Customer;
 
-public interface CustomerDao {
-
+public interface CustomerService {
 	void addCustomer(Customer customer);
 
 	void deleteCustomer(Customer customer);
