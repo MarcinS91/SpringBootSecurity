c543b63549ccd14a05ab48cac82e69065e58a756
MarcinS91 <szewc.marcin91@gmail.com>
2017-01-14 17:33:23 +0100

    commit

--- SpringBootSecurity/src/main/java/com/security/admin/AdminHome.java
+++ SpringBootSecurity/src/main/java/com/security/admin/AdminHome.java
@@ -1,10 +1,20 @@
 package com.security.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.security.service.CustomerService;
 
 @Controller
 @RequestMapping("/home")
 public class AdminHome {
 
+	@Autowired
+	CustomerService customerService;
+	
+	@RequestMapping
+	public String adminPage() {
+		return "admin";
+	}
 }
--- SpringBootSecurity/src/main/java/com/security/controller/HomeController.java
+++ SpringBootSecurity/src/main/java/com/security/controller/HomeController.java
@@ -1,5 +1,30 @@
 package com.security.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HomeController {
 
+	@RequestMapping("/")
+	public String welcome() {
+		return "welcome";
+	}
+
+	@RequestMapping("/login")
+	public String login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, Model model) {
+
+		if (error != null) {
+			model.addAttribute(error, "Niepoprawna nazwa uzytkownika i haslo!");
+		}
+
+		if (logout != null) {
+			model.addAttribute("msg", "Wylogowanie zakonczone z sukcesem!");
+		}
+
+		return "login";
+	}
 }
