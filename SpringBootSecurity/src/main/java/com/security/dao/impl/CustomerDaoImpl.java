f99a6d6d141aee22ac14a8b71af43cfd75dfdbaf
MarcinS91 <szewc.marcin91@gmail.com>
2017-01-14 17:46:40 +0100

    commit

--- SpringBootSecurity/src/main/java/com/security/dao/impl/CustomerDaoImpl.java
+++ SpringBootSecurity/src/main/java/com/security/dao/impl/CustomerDaoImpl.java
@@ -1,5 +1,40 @@
 package com.security.dao.impl;
 
-public class CustomerDaoImpl {
+import java.util.List;
+
+import com.security.dao.CustomerDao;
+import com.security.model.Customer;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+	@Override
+	public void addCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Customer getCustomerById(int customerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> getAllCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer getCustomerByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
