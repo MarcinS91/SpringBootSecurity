d7398640223cc3dbec2950384aff20e311fac29b
MarcinS91 <szewc.marcin91@gmail.com>
2017-01-14 14:15:12 +0100

    commit

---
+++ SpringBootSecurity/src/main/java/com/security/admin/AdminHome.java
@@ -0,0 +1,10 @@
+package com.security.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class AdminHome {
+
+}
